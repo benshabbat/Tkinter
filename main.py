069cc9cd6cf0bb7f2d8b595a1babe57c2c9f016a
benshabbat <benshabbat27@gmail.com>
2023-11-21 21:44:09 +0200

    checking label into labelframe

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import tkinter
 
 window = tkinter.Tk()
-
+window.title("Data Enty Form")
+frame=tkinter.Frame(window)
+frame.pack()
+user_info= tkinter.LabelFrame(frame,text="User Information")
+user_info.grid(row=0,column=0)
+user_first_name= tkinter.Label(user_info,text="First Name")
+user_first_name.grid(row=0,column=0)
 window.mainloop()
