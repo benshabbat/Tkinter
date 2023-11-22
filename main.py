0f8c42726e24e660263ef2530e2bb5a7dc81795c
benshabbat <benshabbat27@gmail.com>
2023-11-22 21:42:30 +0200

    try combobox

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter
+from tkinter import ttk
 
 window = tkinter.Tk()
 window.title("Data Enty Form")
@@ -17,4 +18,9 @@ user_last_name= tkinter.Label(user_info,text="Last Name")
 user_last_name.grid(row=1,column=0)
 user_last_name_entry = tkinter.Entry(user_info)
 user_last_name_entry.grid(row=1 ,column=1)
+
+check=tkinter.Label(user_info,text="Check")
+check.grid(row=2,column=0)
+check_combobox = ttk.Combobox(user_info,values=[1,2,3,4,5,6])
+check_combobox.grid(row=2,column=1)
 window.mainloop()
