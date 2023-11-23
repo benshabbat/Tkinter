65c844a311d7da5f87827be0c5bdc20f9b97ff6b
benshabbat <benshabbat27@gmail.com>
2023-11-23 21:59:53 +0200

    clean code

--- main.py
+++ main.py
@@ -10,57 +10,57 @@ data_info.grid(row=0,column=0 ,padx=20,pady=20)
 
 
 date_label= tkinter.Label(data_info,text="תאריך")
-date_label.grid(row=0,column=1 ,padx=5,pady=5 )
+date_label.grid(row=0,column=1)
 date_entry = tkinter.Entry(data_info)
-date_entry.grid(row=0,column=0 ,padx=5,pady=5)
+date_entry.grid(row=0,column=0)
 
 
 code_hc_label=tkinter.Label(data_info,text="קוד")
-code_hc_label.grid(row=1,column=1 ,padx=5,pady=5)
+code_hc_label.grid(row=1,column=1 )
 code_hc_combobox = ttk.Combobox(data_info,values=["אדום","צהוב"])
-code_hc_combobox.grid(row=1,column=0,padx=5,pady=5)
+code_hc_combobox.grid(row=1,column=0)
 
 
 mana_label= tkinter.Label(data_info,text="מנת")
-mana_label.grid(row=2,column=1,padx=5,pady=5)
+mana_label.grid(row=2,column=1)
 mana_entry = tkinter.Entry(data_info)
-mana_entry.grid(row=2 ,column=0,padx=5,pady=5)
+mana_entry.grid(row=2 ,column=0)
 
 
 number_mana_label= tkinter.Label(data_info,text="מספר מנה")
-number_mana_label.grid(row=3,column=1,padx=5,pady=5)
+number_mana_label.grid(row=3,column=1)
 number_mana_entry = tkinter.Entry(data_info)
-number_mana_entry.grid(row=3 ,column=0,padx=5,pady=5)
+number_mana_entry.grid(row=3 ,column=0)
 
 
 mana_color_label= tkinter.Label(data_info,text="מנת צבע")
-mana_color_label.grid(row=4,column=1,padx=5,pady=5)
+mana_color_label.grid(row=4,column=1)
 mana_color_entry = tkinter.Entry(data_info)
-mana_color_entry.grid(row=4 ,column=0,padx=5,pady=5)
+mana_color_entry.grid(row=4 ,column=0)
 
 
 number_risusim_label= tkinter.Label(data_info,text="מספר ריסוסים")
-number_risusim_label.grid(row=5,column=1,padx=5,pady=5)
+number_risusim_label.grid(row=5,column=1)
 number_risusim_combobox =  ttk.Combobox(data_info,values=[1,2,3,4,5,6,7])
-number_risusim_combobox.grid(row=5 ,column=0,padx=5,pady=5)
+number_risusim_combobox.grid(row=5 ,column=0)
 
 
 viscosity_label= tkinter.Label(data_info,text="צמיגות")
-viscosity_label.grid(row=6,column=1,padx=5,pady=5)
+viscosity_label.grid(row=6,column=1)
 viscosity_entry = tkinter.Entry(data_info)
-viscosity_entry.grid(row=6,column=0,padx=5,pady=5)
+viscosity_entry.grid(row=6,column=0)
 
 
 psi_label= tkinter.Label(data_info,text="PSI")
-psi_label.grid(row=7,column=1,padx=5,pady=5)
+psi_label.grid(row=7,column=1)
 psi_entry = tkinter.Entry(data_info)
-psi_entry.grid(row=7,column=0,padx=5,pady=5)
+psi_entry.grid(row=7,column=0)
 
 
 weight_label= tkinter.Label(data_info,text="משקל")
-weight_label.grid(row=8,column=1,padx=5,pady=5)
+weight_label.grid(row=8,column=1)
 weight_entry = tkinter.Entry(data_info)
-weight_entry.grid(row=8,column=0,padx=5,pady=5)
+weight_entry.grid(row=8,column=0)
 
 
 temp_label= tkinter.Label(data_info,text="טמפרטורה")
